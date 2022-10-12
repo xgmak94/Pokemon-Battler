1084e64b2d1450d853cf0b529ea36e6d93bf084f
xgmak94 <xgmak94@gmail.com>
2022-10-11 22:57:50 -0700

    Refactored context information location

--- contexts/PokemonContext.jsx
+++ contexts/PokemonContext.jsx
@@ -1,4 +1,58 @@
-import react, { createContext } from 'react';
+import { useState, useEffect, useContext, createContext, useMemo } from 'react';
+
+import axios from 'axios';
+
+const pokemonURL = 'https://pokeapi.co/api/v2/';
+
+const SERVER_URL = 'http://localhost:3002'; // local server
+
+const NUM_POKEMON = 151; //151 || 898
 
 export const PokemonContext = createContext();
 
+export function PokemonProvider({ children }) {
+  const [allPokemon, setAllPokemon] = useState([]);
+  const [allAbilities, setAllAbilities] = useState([]);
+  const [allTypes, setAllTypes] = useState([]);
+
+  const pokemonInfo = useMemo(
+    () => ({
+      allPokemon,
+      setAllPokemon,
+      allAbilities,
+      setAllAbilities,
+      allTypes,
+      setAllTypes,
+    }),
+    [allPokemon, allAbilities, allTypes]
+  );
+
+  useEffect(() => {
+    async function getAllPokemon() {
+      let results = await axios.get(`${SERVER_URL}/pokemon`, {
+        params: { limit: NUM_POKEMON },
+      });
+      setAllPokemon(results.data);
+    }
+
+    async function getAllAbilities() {
+      let results = await axios.get(`${SERVER_URL}/abilities`);
+      setAllAbilities(results.data);
+    }
+
+    async function getAllTypes() {
+      let results = await axios.get(`${SERVER_URL}/types`);
+      setAllTypes(results.data);
+    }
+
+    getAllPokemon();
+    getAllAbilities();
+    getAllTypes();
+  }, []);
+
+  return (
+    <>
+      <PokemonContext.Provider value={pokemonInfo}>{children}</PokemonContext.Provider>
+    </>
+  );
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,87 +1,24 @@
 import '../styles/globals.css';
-import react, { useState, useEffect, useContext, createContext, useMemo } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
 import Head from 'next/head';
-import axios from 'axios';
 
 import Navbar from '../components/Navbar';
 
-import { LoginContext } from '../contexts/LoginContext';
-import { PokemonContext } from '../contexts/PokemonContext';
-
-const pokemonURL = 'https://pokeapi.co/api/v2/';
-
-const SERVER_URL = 'http://localhost:3002';
-const NUM_POKEMON = 151; //151 || 898
+import { LoginProvider } from '../contexts/LoginContext';
+import { PokemonProvider } from '../contexts/PokemonContext';
 
 export default function MyApp({ Component, pageProps }) {
-  const [accountInfo, setAccountInfo] = useState({
-    username: '',
-    password: '',
-  });
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [allPokemon, setAllPokemon] = useState([]);
-  const [allAbilities, setAllAbilities] = useState([]);
-  const [allTypes, setAllTypes] = useState([]);
-
-  const userInfo = useMemo(
-    () => ({
-      accountInfo,
-      setAccountInfo,
-      loggedIn,
-      setLoggedIn,
-    }),
-    [accountInfo, loggedIn]
-  );
-
-  const pokemonInfo = useMemo(
-    () => ({
-      allPokemon,
-      setAllPokemon,
-      allAbilities,
-      setAllAbilities,
-      allTypes,
-      setAllTypes,
-    }),
-    [allPokemon, allAbilities, allTypes]
-  );
-
-  useEffect(() => {
-    async function getAllPokemon() {
-      let results = await axios.get(`${SERVER_URL}/pokemon`, {
-        params: { limit: NUM_POKEMON },
-      });
-      setAllPokemon(results.data);
-    }
-
-    async function getAllAbilities() {
-      let results = await axios.get(`${SERVER_URL}/abilities`);
-      setAllAbilities(results.data);
-    }
-
-    async function getAllTypes() {
-      let results = await axios.get(`${SERVER_URL}/types`);
-      setAllTypes(results.data);
-    }
-
-    getAllPokemon();
-    getAllAbilities();
-    getAllTypes();
-  }, []);
-
   return (
     <>
       <Head>
         <title>Pokemon battle</title>
         <meta property="og:title" content="My page title" key="title" />
       </Head>
-      <LoginContext.Provider value={userInfo}>
-        <PokemonContext.Provider value={pokemonInfo}>
+      <LoginProvider>
+        <PokemonProvider>
           <Navbar />
           <Component {...pageProps} />
-        </PokemonContext.Provider>
-      </LoginContext.Provider>
+        </PokemonProvider>
+      </LoginProvider>
     </>
   );
 }
