61929a0864a5bc6f87a9f2d731859e4a3028655d
xgmak94 <xgmak94@gmail.com>
2022-10-22 14:09:54 -0700

    Fixing redirect after login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,7 @@ import prisma from '../../../utils/ConnectPrisma';
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  site: process.env.NEXTAUTH_URL,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
